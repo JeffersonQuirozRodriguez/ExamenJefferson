289466bc927aeb29a46ad46c932993c2fd769142
Jefferson Quiroz <jeffersonquirozrodriguez@gmail.com>
2025-07-31 22:50:37 -0500

    Creacion de tablas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args){
-
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("AcademiaPU");
+        EntityManager em = emf.createEntityManager();
     }
 }
