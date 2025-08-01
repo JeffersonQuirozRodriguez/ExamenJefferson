b8f658dbac46ea786a437c2f57cea2d7e822eac4
Jefferson Quiroz <jeffersonquirozrodriguez@gmail.com>
2025-07-31 23:27:37 -0500

    Error corregido

--- src/main/java/org/example/util/InicioSesion.java
+++ src/main/java/org/example/util/InicioSesion.java
@@ -3,15 +3,12 @@ package org.example.util;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
 public class InicioSesion {
     private static final EntityManagerFactory emf;
 
     static {
         try {
-            emf = Persistence.createEntityManagerFactory("AcademiaPU"); // Usa tu persistence.xml
+            emf = Persistence.createEntityManagerFactory("AcademiaPU");
         } catch (Throwable ex) {
             System.err.println("No se puede iniciar sesion: " + ex);
             throw new ExceptionInInitializerError(ex);
